4132e1def19261559bfde8999f5ecbf0d0ed70c4
Ainur Dzhaianbaeva and Faiza Husain <faiza@>
2018-01-17 16:23:04 -0800

    adds another div to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,13 @@
 //var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
 function puzzle(inputString){
-  var newphrase = str.replace(/[aeiou]/gi, "*");
+  var newphrase = inputString.replace(/[aeiou]/gi, "*");
   return newphrase;
 }
 
 function toggle() {
   $("#wordpuzzle").toggle();
-  $("#puzzle").toggle()
+  $("#initially-hidden").toggle();
 }
 
 // JQUERY
@@ -16,9 +16,10 @@ $(document).ready(function() {
     $("#wordpuzzle").submit(function(event) {
       event.preventDefault();
 
-      var userInput = $("input#" + puzzle).val();
-      $("#puzzle").text(puzzle(userInput));
+      var userInput = $("#string").val();
+      $("#initially-hidden").text(puzzle(userInput));
+      toggle();
 
-      $("#").show
+
     });
   });
